456916b07f532f8852f095189a72f3f4ee7b0b2f
Lance <Lance@Lances-Macbook-Air.local>
2016-05-24 13:04:26 -0600

    all the loops work

---
+++ loops.js
@@ -0,0 +1,32 @@
+console.log('Problem 1')
+for (var i = 0; i < 6; i++){
+	console.log(i*100)
+}
+console.log('Problem 2')
+for (var i = 0; i < 7; i++){
+	console.log(Math.pow(2,i))
+}
+console.log('Problem 3')
+for (var i = 0; i < 3; i++){
+	for (var j = 0; j < 3; j++){
+		console.log(i+1)
+	}
+}
+console.log('Problem 4')
+for (var i = 0; i < 6; i++){
+	console.log(i*2)
+}
+console.log('Problem 5')
+for (var i = 0; i < 5; i++){
+	console.log((i+1)*3)
+}
+console.log('Problem 6')
+for (var i = 9; i >= 0; i--){
+	console.log(i)
+}
+console.log('Problem 7')
+for (var i = 0; i < 3; i++){
+	for (var j = 0; j < 4; j++){
+		console.log(j)
+	}
+}
